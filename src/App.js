8f6a2917c9baa2d38040c96c1ad3bf57ec1aab4c
Kelly Cook <kellyrcook1980@gmail.com>
2021-06-23 12:54:04 -0600

    set up app.js and nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../../pages/home';
+import About from '../../pages/about';
+import Contact from '../../pages/contact';
+import Apply from '../../pages/apply';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router basename={process.env.PUBLIC_URL}>
+     <>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/ContactUs" component={Contact} />
+        <Route exact path="/ApplyForAssistance" component={Apply} />
+     </>
+   </Router>
+
   );
 }
 
---
+++ src/components/nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap';
+
+
+
+export default function index() {
+    return (
+        <Navbar>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav"> 
+                <Navbar.Link href="#Home">Home</Navbar.Link>
+                <Navbar.Link href="#About">About</Navbar.Link>
+                <Navbar.Link href="#Contact">Contact Us</Navbar.Link>
+                <Navbar.Link href="#Apply">Apply</Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+
+        
+    )
+}
+
